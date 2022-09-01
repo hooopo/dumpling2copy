2ce8d9611a8263bd8f0ec0f68fe27e1884b35396
Hooopo Wang <hoooopo@gmail.com>
2022-09-02 03:45:33 +0800

    extract column names from csv

--- run.rb
+++ run.rb
@@ -9,7 +9,7 @@ Dotenv.load
 
 clean_cmd = "psql discourse -f clean_all.sql"
 puts clean_cmd 
-system(clean_cmd)
+system(clean_cmd) rescue nil
 
 skip_list = %w[
   tx
@@ -19,9 +19,7 @@ skip_list = %w[
   user_options
   post_search_data
   post_action_types
-  topic_users
   user_api_keys
-  category_featured_topics
   topic_search_data
 ]
 
@@ -34,7 +32,8 @@ Dir.glob("#{csv_dir}/*.csv").each do |file|
     next
   end
   begin 
-    cmd = %Q{psql discourse -e -c "SET session_replication_role = replica;" -c "set role to discourse;" -c "\\copy #{table_name} FROM '#{file}' WITH HEADER NULL '\\N' DELIMITER E',' QUOTE E'\\'' ESCAPE E'\\\\\\\\' CSV;"}
+    column_names = `head -1 #{file}`.strip
+    cmd = %Q{psql discourse -e -c "SET session_replication_role = replica;" -c "set role to discourse;" -c "\\copy #{table_name} (#{column_names}) FROM '#{file}' WITH HEADER NULL '\\N' DELIMITER E',' QUOTE E'\\'' ESCAPE E'\\\\\\\\' CSV;"}
     puts cmd 
     system(cmd)
   rescue 
