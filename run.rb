9e2f4a9fe773dfb6c8142c9922b663ab08e3fe75
Hooopo Wang <hoooopo@gmail.com>
2022-09-04 22:40:20 +0800

    add user_options

--- run.rb
+++ run.rb
@@ -43,6 +43,48 @@ Dir.glob("#{csv_dir}/*.000000000.csv").each do |file|
           csv_w << h.values_at(*columns)
         end
       end
+    elsif table_name == 'user_options'
+      original_file = file 
+      columns = ["user_id",
+        "mailing_list_mode",
+        "email_digests",
+        "external_links_in_new_tab",
+        "enable_quoting",
+        "dynamic_favicon",
+        "disable_jump_reply",
+        "automatically_unpin_topics",
+        "digest_after_minutes",
+        "auto_track_topics_after_msecs",
+        "new_topic_duration_minutes",
+        "last_redirected_to_top_at",
+        "email_previous_replies",
+        "email_in_reply_to",
+        "like_notification_frequency",
+        "mailing_list_mode_frequency",
+        "include_tl0_in_digests",
+        "notification_level_when_replying",
+        "theme_key_seq",
+        "allow_private_messages",
+        "homepage_id",
+        "theme_ids",
+        "hide_profile_and_presence",
+        "text_size_key",
+        "text_size_seq",
+        "email_level",
+        "email_messages_level",
+        "title_count_mode_key",
+        "enable_defer"
+      ]
+
+      file = file.gsub("000000000", "111111111")
+
+      CSV.open(file, "w", headers: columns, write_headers: true, quote_char: "'", force_quotes: true, write_nil_value: '\N') do |csv_w|
+        CSV.open(original_file,  headers: true, quote_char: "'", col_sep: ',', liberal_parsing: true).each do |csv_r|
+          h = csv_r.to_h 
+          h["theme_ids"] = encoder.encode(JSON.parse(h["theme_ids"]))
+          csv_w << h.values_at(*columns)
+        end
+      end
     else
       puts "skipped #{table_name}"
       next
